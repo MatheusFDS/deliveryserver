bc7c0de555ab34ff24419adfce608b64a97bf625
Matheus Ferreira da Silva <matheus@MacBook-Pro-de-Matheus.local>
2025-07-31 02:42:41 -0300

    ajuste dto login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, IsOptional, MinLength } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'O email deve ser um endereço de e-mail válido.' })
   email: string;
+
+  @IsString({ message: 'A senha deve ser uma string.' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' }) // Exemplo de validação de tamanho
   password: string;
+
+  @IsString({ message: 'O domínio deve ser uma string.' })
+  @IsOptional() // Permite que o campo seja opcional
   domain?: string;
 }
