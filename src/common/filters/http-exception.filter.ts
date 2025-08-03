dd38ccb83cad5c7e4c95264643ff2e50a2e3eb9d
Matheus Ferreira da Silva <matheus@MacBook-Pro-de-Matheus.local>
2025-08-03 00:36:15 -0300

    chore: ajustando express

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,33 +1,32 @@
-// src/common/filters/http-exception.filter.ts
 import {
   Catch,
   ArgumentsHost,
   HttpException,
   ExceptionFilter,
-  HttpStatus,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+
+    const response = ctx.getResponse();
+    const request = ctx.getRequest() as any; // força o TS a permitir acesso a url
     const status = exception.getStatus();
 
-    // Mensagem de erro padrão ou a que vem da exceção
     const errorResponse = exception.getResponse();
     const message =
-      typeof errorResponse === 'object' && 'message' in errorResponse
-        ? errorResponse.message
+      typeof errorResponse === 'object' &&
+      errorResponse !== null &&
+      'message' in errorResponse
+        ? (errorResponse as any).message
         : exception.message;
 
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
-      path: request.url,
-      message: message, // Garante que a mensagem seja sempre retornada
+      path: request.url, // agora funciona mesmo sem tipagem
+      message,
     });
   }
 }
