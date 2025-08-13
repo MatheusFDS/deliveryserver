c92ce0bc3a28b1cb76b71535a29ff3573d89874f
Matheus Ferreira da Silva <matheus@MacBook-Pro-de-Matheus.local>
2025-08-13 15:07:00 -0300

    fix: corrigindo bug

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -3,16 +3,17 @@ import { DriversService } from './drivers.service';
 import { DriversController } from './drivers.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthModule } from '../auth/auth.module';
-import { UsersModule } from 'src/users/users.module'; // Adicionado
-import { TenantModule } from 'src/tenant/tenant.module'; // Adicionado
+import { UsersModule } from 'src/users/users.module';
+import { TenantModule } from 'src/tenant/tenant.module';
 
 @Module({
   imports: [
-    forwardRef(() => AuthModule), // Adicionado forwardRef
-    forwardRef(() => UsersModule), // Adicionado
-    forwardRef(() => TenantModule), // Adicionado
+    forwardRef(() => AuthModule),
+    forwardRef(() => UsersModule),
+    forwardRef(() => TenantModule),
   ],
   controllers: [DriversController],
   providers: [DriversService, PrismaService],
+  exports: [DriversService], // Adicionar esta linha para exportar o serviço
 })
 export class DriversModule {}
